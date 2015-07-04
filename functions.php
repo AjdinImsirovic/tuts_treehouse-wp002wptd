30fc854da7cf94a2adb1fdf3b82f670cfa84611f
Ajdin Imsirovic <ajdika.i@gmail.com>
2015-07-04 15:54:00 +0000

    In the previous commit I didn't include add_action(), so I'm doing it now

--- functions.php
+++ functions.php
@@ -20,3 +20,4 @@ function wpt_theme_styles() {
  wp_enqueue_style( 'normalize_css', 'http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic' );
  wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
 }
+add_action( 'wp_enqueue_scripts', 'wpt_theme_styles' );
